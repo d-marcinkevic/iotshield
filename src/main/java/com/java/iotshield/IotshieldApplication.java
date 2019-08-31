7a0c073bf3bec54227dcd9962aa2519331d2075c
d.marcinkevic <d.marcinkevic@adform.com>
2019-08-31 15:21:16 +0300

    initial

---
+++ src/main/java/com/java/iotshield/IotshieldApplication.java
@@ -0,0 +1,13 @@
+package com.java.iotshield;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotshieldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IotshieldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/java/iotshield/IotshieldApplicationTests.java
@@ -0,0 +1,16 @@
+package com.java.iotshield;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IotshieldApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
